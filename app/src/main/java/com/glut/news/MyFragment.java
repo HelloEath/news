a946560a42231b7d729fb19321a98dec144a7e0e
HelloEath <18593233582@163.com>
2018-01-22 20:36:52 +0800

    first commit

---
+++ app/src/main/java/com/glut/news/MyFragment.java
@@ -0,0 +1,20 @@
+package com.glut.news;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yy on 2018/1/22.
+ */
+public class MyFragment extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.fragment_my,container,false);
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/glut/news/VideoFragment.java
@@ -0,0 +1,19 @@
+package com.glut.news;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yy on 2018/1/22.
+ */
+public class VideoFragment extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_video,container,false);
+        return view;
+    }
+}
